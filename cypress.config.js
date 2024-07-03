78efeb5ab0650759644fcfd63ab35a66755db62d
Benjamin_79 <shadowboxer79@hotmail.com>
2024-07-03 12:34:36 -0400

    Updated configuration file

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("cypress");
 const createBuilder = require('@bahmutov/cypress-esbuild-preprocessor');
-const { json } = require("express");
+
 const addCucumberpreprocessor = require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin
 const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin
 module.exports = defineConfig({
